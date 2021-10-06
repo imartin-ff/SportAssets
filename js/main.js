76b5fb26dce0dd06ed40c8513aceda015fc7eaae
MaRTiN <53973885+imartin-ff@users.noreply.github.com>
2021-10-06 16:14:46 +0300

    16:14

--- js/main.js
+++ js/main.js
@@ -8,26 +8,33 @@ $(function () {
         spaceBetween: 13,
         slideClass: 'main-screen__cards-card',
         wrapperClass: 'main-screen__cards-wrapper',
+        loop: false,
+        autoplay: {
+            delay: 2000,
+        },
         // Responsive breakpoints
         breakpoints: {
             320:{
                 centeredSlides: true,
                 slidesPerView: 1,
-                spaceBetween: 0,
                 spaceBetween: 10,
                 loop: true,
                 loopAdditionalSlides: 3,
+                
+               
             },
             
             780: {
                 centeredSlides: false,
                 slidesPerView: 5,
                 spaceBetween: 10,
-                loop: false
+                loop: false,
+                
             },
             1024:{
                 slidesPerView: 5,
-                spaceBetween: 14
+                spaceBetween: 14,
+
             }
             
         }
@@ -209,18 +216,66 @@ $(function () {
       });
 
 
-      $( ".accordion" ).accordion({
+      
+
+      $('.select').each(function() {
+        const _this = $(this),
+            selectOption = _this.find('option'),
+            selectOptionLength = selectOption.length,
+            selectedOption = selectOption.filter(':selected'),
+            duration = 250; // длительность анимации 
+    
+        _this.hide();
+        _this.wrap('<div class="select"></div>');
+        $('<div>', {
+            class: 'new-select',
+            text: _this.children('option:disabled').text()
+        }).insertAfter(_this);
+    
+        const selectHead = _this.next('.new-select');
+        $('<div>', {
+            class: 'new-select__list'
+        }).insertAfter(selectHead);
+    
+        const selectList = selectHead.next('.new-select__list');
+        for (let i = 1; i < selectOptionLength; i++) {
+            $('<div>', {
+                class: 'new-select__item',
+                html: $('<span>', {
+                    text: selectOption.eq(i).text()
+                })
+            })
+            .attr('data-value', selectOption.eq(i).val())
+            .appendTo(selectList);
+        }
+    
+        const selectItem = selectList.find('.new-select__item');
+        selectList.slideUp(0);
+        selectHead.on('click', function() {
+            if ( !$(this).hasClass('on') ) {
+                $(this).addClass('on');
+                selectList.slideDown(duration);
+    
+                selectItem.on('click', function() {
+                    let chooseItem = $(this).data('value');
+    
+                    $('select').val(chooseItem).attr('selected', 'selected');
+                    selectHead.text( $(this).find('span').text() );
+    
+                    selectList.slideUp(duration);
+                    selectHead.removeClass('on');
+                });
+    
+            } else {
+                $(this).removeClass('on');
+                selectList.slideUp(duration);
+            }
+        });
+    });
+
+    $( ".accordion" ).accordion({
         heightStyle: 'content'
       });
-
-
-      const inputRange = document.getElementById('sum');
-      const sum = $('.sum')[0];
-      const result = $('.result')[0];
-      inputRange.addEventListener('change', function(){
-          sum.innerText = `${inputRange.value}₽`;
-          result.innerText = `+ ${inputRange.value * 3.3}₽`;
-      })
 
 
 })
